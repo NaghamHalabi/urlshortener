b1723095d7b105e01c97eaca1cbb5f5d5b27b474
Hassan Salem <h.salem7788@gmail.com>
2018-07-05 11:09:26 +0300

    add default values for request

--- src/Core/HttpClientAdapter.php
+++ src/Core/HttpClientAdapter.php
@@ -12,6 +12,13 @@ class HttpClientAdapter implements HttpClientInterface
         $this->client = $client?: new Client;
     }
 
+    /**
+     * Build url with parameters
+     *
+     * @param String $url
+     * @param Array $parameters
+     * @return String
+     */
     public function buildUrl($url, $parameters = [])
     {
         return $url.'?'.join('&', array_map(function ($key, $value) {
@@ -19,7 +26,16 @@ class HttpClientAdapter implements HttpClientInterface
         }, array_keys($parameters), $parameters));
     }
 
-    public function request($url, $parameters, $verb)
+    /**
+     * Sends the request to the url
+     *
+     * @param String $url
+     * @param Array $parameters
+     * @param String $verb
+     * @param Array $headers
+     * @return JSON Object
+     */
+    public function request($url, $parameters = [], $verb = 'get', $headers = [])
     {
         $url = $this->buildUrl($url, $parameters);
     }
