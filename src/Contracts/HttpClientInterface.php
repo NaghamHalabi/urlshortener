36667072769a15a523edca19dd7601c087ba56c5
Hassan Salem <h.salem7788@gmail.com>
2018-07-05 22:58:22 +0300

    add methods to the interface

--- src/Contracts/HttpClientInterface.php
+++ src/Contracts/HttpClientInterface.php
@@ -4,5 +4,10 @@ namespace hassanalisalem\urlshortener\Contracts;
 
 interface HttpClientInterface
 {
-
+    public function prepareRequest($verb, $url, $parameters = []);
+    public function setHeaders($headers);
+    public function setBody($body);
+    public function send();
+    public function getStatusCode();
+    public function getBody();
 }
