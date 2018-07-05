c7bc372c11a98caa10b89631cae8195d8e4988df
Hassan Salem <h.salem7788@gmail.com>
2018-07-05 23:00:36 +0300

    add some test

--- tests/AbcTest.php
+++ tests/AbcTest.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace hassanalisalem\urlshortener\Tests;
+
 use hassanalisalem\urlshortener\Core\HttpClientAdapter;
+use hassanalisalem\urlshortener\Drivers\Bitly;
 
 class AbcTest extends TestCase
 {
@@ -9,8 +11,18 @@ class AbcTest extends TestCase
     public function testHassan()
     {
         $httpClient = new HttpClientAdapter;
-        $this->assertEquals($httpClient->buildUrl('abc', ['a' => 1, 'b' => 2]), 'abc?a=1&b=2');
-        $this->assertNotEquals($httpClient->buildUrl(['a' => 1, 'b' => 2]), 'abc?a=1&b=3');
+        $response = $httpClient->prepareRequest('get', 'http://www.google.com')->send();
+        $this->assertEquals($response->getStatusCode(), 200);
+    }
+
+    public function testBitly()
+    {
+        $httpClient = new HttpClientAdapter;
+        $bitly = new Bitly([
+            'access_token' => '89b2d7baf3f03936ea4e3e9400cb18a91fb2bd92',
+        ], $httpClient);
+        $url = $bitly->getShortUrl('http://www.google.com');
+        $this->assertStringStartsWith('http', $url);
     }
 
 }
